fc97df582f95a57ed8ecf45725d3fbe28e4d2322
Bo-Liu-TUM <bo.liu@tum.de>
2026-03-16 03:24:43 +0100

    delete

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', () => {
-  const current = location.pathname.split('/').pop() || 'index.html';
-  document.querySelectorAll('.nav-center a').forEach(a => {
-    if (a.getAttribute('href') === current) a.classList.add('active');
-  });
-});
